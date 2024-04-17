b0be6c96b089c46134c05aa83542f59b83a30c95
Bircheses <kbrzoza03@outlook.com>
2024-04-17 22:07:37 +0200

    final program v4

--- Sources/Counter.h
+++ Sources/Counter.h
@@ -3,16 +3,18 @@
 
 #include <windows.h>
 #include <chrono>
-
+/**
+ * Counter class that counts time
+ */
 class Counter {
 private:
-    std::chrono::high_resolution_clock::time_point startTime;
-    std::chrono::high_resolution_clock::time_point endTime;
-    double elapsedTime{};
+    std::chrono::high_resolution_clock::time_point startTime; //start time point
+    std::chrono::high_resolution_clock::time_point endTime; //end time point
+    double elapsedTime{}; // time between start and end points
 public:
-    void start();
-    void stop();
-    [[nodiscard]] double getElapsedTime() const;
+    void start(); //starts clock
+    void stop(); //stops clock
+    [[nodiscard]] double getElapsedTime() const; //returns value of time passed between start and end point in milliseconds
 };
 
 
